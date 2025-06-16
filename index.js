f083f8c73bafefe7c684fcc909fec6f998b005e7
lolodev <horaciodev1706@gmail.com>
2025-06-16 19:24:30 -0300

    Create an unordered list element (<ul>) with id="ul-el" and grab the unordered list and store it in a const variable called ulEl

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function () {
-    console.log("Button clicked!");
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
 });
+
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i]);
+}
