f266eed1988559cceaf82dca3be04bbcdb566762
lolodev <horaciodev1706@gmail.com>
2025-06-17 20:04:10 -0300

    Create a variable, listItems, to hold all the HTML for the list items Assign it to an empty string to begin with Add the item to the listItems variable instead of the ulEl.innerHTML Render the listItems inside the unordered list using ulEl.innerHTML

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    inputEl.value = "";
+    renderLeads();
 });
 
-let listItems = "";
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li>${myLeads[i]}</li>`;
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `<li>
+                          <a target='_blank' href='${myLeads[i]}'>
+                             ${myLeads[i]}
+                          </a>
+                      </li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
-
-ulEl.innerHTML = listItems;
